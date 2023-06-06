b1b139a142f19b70f39ab5e200a1f457fd1fa730
anowarzz <anowarhosen444@gmail.com>
2023-06-06 18:34:08 +0600

    added display 2 background

--- src/app/components/Display2/Display2.jsx
+++ src/app/components/Display2/Display2.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Display2.module.css'
+import Image from 'next/image';
+import JeansCollect from '../../../../public/images/jeans-collection.jpg'
+import svgShape  from '../../../../public/images/blob.svg'
+
 
 const Display2 = () => {
     return (
-        <div className={` h-96 ${styles.display2}`}>
-            
+        <>
+        <div className={`h-64 relative ${styles.display2}`}>
+   
+         
+
         </div>
+            {/* <Image alt='Jeans' src={JeansCollect} className='h-full lg:h-[700px] w-full'/> */}
+        </>
     );
 };
 
